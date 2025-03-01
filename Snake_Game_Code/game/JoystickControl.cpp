6cd624e08fe3f690e0488b4c81a0e980b562b5fe
AbdSh17 <abdalraheem200417a@gmail.com>
2025-03-01 23:47:27 +0200

    turn the analog code into library .h

---
+++ Snake_Game_Code/game/JoystickControl.cpp
@@ -0,0 +1,17 @@
+#include "JoystickControl.h"
+const int move_range = 20;
+
+void setupJoystick(int xaxis, int yaxis, int sw) {
+  pinMode(xaxis, INPUT);
+  pinMode(yaxis, INPUT);
+  pinMode(sw, INPUT);
+  Serial.begin(9600);
+}
+
+void get_xy(int *XY) {
+  int X = analogRead(xaxis);
+  int Y = analogRead(yaxis);
+
+  XY[0] = map(X, 0, 1023, 0, 360);
+  XY[1] = map(Y, 0, 1023, 0, 360);
+}
--- Snake_Game_Code/game/game.ino
+++ Snake_Game_Code/game/game.ino
@@ -1,18 +1,11 @@
-#include <math.h>
+#include "JoystickControl.h"
 
 const int xaxis = A0;
 const int yaxis = A1;
-const int sw = 2;
-
-const int move_range = 20; 
+const int sw = 2;    
 
 void setup() {
-  pinMode(xaxis, INPUT);
-  pinMode(yaxis, INPUT);
-  pinMode(sw, INPUT);
-
-  Serial.begin(9600);
-
+  setupJoystick(A0, A1, 2);
 }
 
 void loop() {
@@ -23,46 +16,32 @@ void loop() {
 
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
+    get_Direction();
     
-    int XY[2];  
-    get_xy(XY); 
-    int x = XY[0], y = XY[1];
-
-    if(abs(x - 180) > move_range || abs(y - 180 ) > move_range) {
-        if(abs(x - 180 ) > abs(y - 180 )) {
-          if(x > 180) {
-            Serial.println("LEFT");
-          }
-          else {
-            Serial.println("RIGHT");
-          }
-        }
-
-        else {
-          if(y > 180) {
-            Serial.println("UP");
-          }
-          else {
-            Serial.println("DOWN");
-          }
-        }
-    }
-
-
-
-    // Serial.print("Y: ");
-    // Serial.print(XY[1]);
-    // Serial.print("  X: ");
-    // Serial.println(XY[0]);
   }
 }
 
-void get_xy(int *XY) {
-  int X = analogRead(xaxis);  
-  int Y = analogRead(yaxis);  
+void get_Direction() {
+    int XY[2];
+    get_xy(XY);
+    int x = XY[0], y = XY[1];
 
-  XY[0] = map(X, 0, 1023, 0, 360);
-  XY[1] = map(Y, 0, 1023, 0, 360);
-
+    if (abs(x - 180) > move_range || abs(y - 180) > move_range) {
+      if (abs(x - 180) > abs(y - 180)) {
+        if (x > 180) {
+          Serial.println("LEFT");
+        }
+        else {
+          Serial.println("RIGHT");
+        }
+      }
+      else {
+        if (y > 180) {
+          Serial.println("UP");
+        }
+        else {
+          Serial.println("DOWN");
+        }
+      }
+    }
 }
-
