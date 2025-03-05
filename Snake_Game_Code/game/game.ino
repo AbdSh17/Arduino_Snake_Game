4c2fb96a9d1c5a41ddce17bc2dc63c6fd6511938
AbdSh17 <abdalraheem200417a@gmail.com>
2025-03-05 23:21:10 +0200

    Fully working code

--- Snake_Game_Code/game/game.ino
+++ Snake_Game_Code/game/game.ino
@@ -13,6 +13,10 @@
 
 #define ROWS 8
 #define COLUMNS 8
+
+const byte apple = 1;
+const byte snake = 2;
+const byte none = 0;
 // =============================== DEFINE ===============================
 
 // =============================== CONST ===============================
@@ -20,8 +24,8 @@ const int xaxis = A0;
 const int yaxis = A1;
 const int sw = 2;
 
-const long appleInterval = 1500;
-const long moveInterval = 300;
+const long appleInterval = 8000;
+const long moveInterval = 1;
 // =============================== CONST ===============================
 
 // =============================== GLOBAL ===============================
@@ -38,14 +42,14 @@ LedControl lc = LedControl(11, 13, 10, 1);
 // =============================== DECLARATOIN ===============================
 int get_Direction();
 void print_location(int, int);
-byte get_user_direction();
 bool isPoint(int, int);
-void go_down();
-void go_up();
-void go_left();
-void go_right();
+bool go_down();
+bool go_up();
+bool go_left();
+bool go_right();
 void switchPoints(int, int, int, int);
 void set_apple();
+void printGameOver();
 
 
 // =============================== DECLARATOIN ===============================
@@ -54,10 +58,7 @@ void set_apple();
 typedef struct leds *led;
 
 struct leds {
-  led next;
-  led previous;
-  bool state;
-  byte nextStep;
+  byte state;
   byte previousLoc;
 }; 
 
@@ -86,10 +87,8 @@ void handleSwitch() {
 void setLedStates() {
   for (int i = 0; i < ROWS; i++){
     for (int j = 0; j < COLUMNS; j++){
-      ledState[i][j].state = false;
-      ledState[i][j].nextStep = STABLE;
-      ledState[i][j].next = nullptr;
-      ledState[i][j].previous = nullptr;
+      ledState[i][j].state = none;
+      ledState[i][j].previousLoc = 0;
     }
   }
 }
@@ -117,59 +116,91 @@ void setup() {
 
   setLedStates();
 
-    ledState[3][0].state = true;
-    ledState[5][0].state = true;
-    lc.setLed(0, 5, 0, true);
-    lc.setLed(0, 3, 0, true);
+    // ledState[2][0].state = true;
+    // ledState[4][0].state = true;
+    // ledState[5][5].state = true;
+    // lc.setLed(0, 5, 0, true);
+    // lc.setLed(0, 3, 0, true);
+    // lc.setLed(0, 5, 5, true);
 
-    ledState[0][3].state = true;
-    ledState[0][5].state = true;
-    lc.setLed(0, 0, 3, true);
-    lc.setLed(0, 0, 5, true);
+    // ledState[0][2].state = true;
+    // ledState[0][4].state = true;
+
+    // lc.setLed(0, 0, 2, true);
+    // lc.setLed(0, 0, 4, true);
 
     lc.setLed(0, userY, userX, true);
-    ledState[userY][userX].state = true;
+    ledState[userY][userX].state = snake;
 
+}
+
+void restart() {
+  clearMatrix();
+  setLedStates();
+  userX = userY = 0;
+  lc.setLed(0, userY, userX, true);
+  ledState[userY][userX].state = snake;
 }
 
 void loop() {
 
-  static unsigned long previousMillis = 0;
-  unsigned long currentMillis = millis();   
+  static unsigned long previousMillisApple = 0;
+  static unsigned long previousMillisMove = 0;
 
-  if (currentMillis - previousMillis >= moveInterval) {
-    previousMillis = currentMillis;
-    // set_apple();
-    int analogDirection = get_Direction();
+  unsigned long currentMillis = millis();  
 
-    if(analogDirection == RIGHT) {
-      go_right();
+
+  if (currentMillis - previousMillisApple >= appleInterval) {
+    previousMillisApple = currentMillis;
+    set_apple();
+  }
+
+  if (currentMillis - previousMillisMove >= moveInterval) {
+    previousMillisMove = currentMillis;
+    
+    int joystickDirection = get_Direction();
+    static int lastDirection = joystickDirection;
+
+    if (joystickDirection == STABLE) {
+      joystickDirection = lastDirection;
+
     }
 
-    else if (analogDirection == LEFT) {
-      go_left();
+    bool lost = false;
+
+    if(joystickDirection == RIGHT) {
+      lastDirection = RIGHT;
+      lost = go_right();
     }
 
-    else if (analogDirection == UP) {
-      go_up();
+    else if (joystickDirection == LEFT) {
+      lastDirection = LEFT;
+      lost = go_left();
     }
 
-    else if (analogDirection == DOWN) {
-      go_down();
+    else if (joystickDirection == UP) {
+      lastDirection = UP;
+      lost = go_up();
+    }
+
+    else if (joystickDirection == DOWN) {
+      lastDirection = DOWN;
+      lost = go_down();
+    }
+
+    Serial.print("DIRECTION:   ");
+    Serial.println(joystickDirection);
+
+    if (lost) {
+      printGameOver();
+      restart();
+      joystickDirection = lastDirection = STABLE;
     }
   }
 }
 
 void set_apple() {
   static int row = 1, column = 0;
-
-  lc.setLed(0, row, column, false);
-
-  static bool wait = true;
-  wait = !wait;
-  if (wait) {
-    return;
-  }
 
   while (1) {
     row = random(0, 8);
@@ -178,46 +209,29 @@ void set_apple() {
       break;
   }
   lc.setLed(0, row, column, true);
+  ledState[row][column].state = apple;
 }
 
 void switchPoints(int userX0, int userY0, int userX1, int userY1) {
   lc.setLed(0, userY0, userX0, false);
-  ledState[userY0][userX0].state = false;
+  ledState[userY0][userX0].state = none;
   
   lc.setLed(0, userY1, userX1, true);
-  ledState[userY1][userX1].state = true;
+  ledState[userY1][userX1].state = snake;
 
   ledState[userY1][userX1].previousLoc = ledState[userY0][userX0].previousLoc;
-  // ledState[userY0][userX0].previousLoc = 0;
 
-  if (ledState[userY0][userX0].previous) {
-    ledState[userY0][userX0].next = &ledState[userY1][userX1];
-    ledState[userY1][userX1].previous = &ledState[userY0][userX0];
-  }
-  else {
-    ledState[userY0][userX0].next = nullptr;
-    ledState[userY1][userX1].previous = nullptr;
-  }
 }
 
-/*
-RIGHT: switchPoints(userX - i, userY, userX - i + 1, userY);
-LEFT: switchPoints(userX + i, userY, userX + i - 1, userY);
-UP: switchPoints(userX, userY + i, userX, userY + i - 1);
-DOWN: switchPoints(userX, userY - i, userX, userY - i + 1);
-*/
+bool go_right() {
 
-
-void go_right() {
-  userDirection = get_user_direction();
-  if (userDirection == STABLE) {
-    userDirection = RIGHT;
-  }
-  if (userDirection == LEFT) {
-    return;
+  if (userX == 7) {
+    return true;
   }
 
-  if (!isPoint(userY, userX + 1)) {
+  byte state = getState(userY, userX + 1);
+
+  if (!state) {
     switchPoints(userX, userY, userX + 1, userY);
     int tempY = userY, tempX = userX;
     userX++;
@@ -226,7 +240,20 @@ void go_right() {
     }
 
     for(int i= 1; i < length; i++) {
-      if (ledState[tempY][tempX].previousLoc == LEFT) {
+
+      if (!ledState[tempY][tempX].previousLoc) {
+        ledState[tempY][tempX].state = none;
+      }
+
+      if (ledState[tempY][tempX].previousLoc == RIGHT) {
+        switchPoints(tempX + 1, tempY, tempX, tempY);
+        if (ledState[tempY][tempX].previousLoc) {
+          ledState[tempY][tempX].previousLoc = RIGHT;
+        }
+        tempX++;
+      }
+
+      else if (ledState[tempY][tempX].previousLoc == LEFT) {
         switchPoints(tempX - 1, tempY, tempX, tempY);
         if (ledState[tempY][tempX].previousLoc) {
           ledState[tempY][tempX].previousLoc = LEFT;
@@ -241,84 +268,185 @@ void go_right() {
         }
         tempY--;
       }
-    }
-      // ledState[userY][userX].previous->nextStep = RIGHT;
-      if(ledState[userY][userX].previous) {
-        Serial.println(ledState[userY][userX].previous->next->state);
+
+      else if(ledState[tempY][tempX].previousLoc == DOWN) {
+        switchPoints(tempX, tempY + 1, tempX, tempY);
+        if (ledState[tempY][tempX].previousLoc) {
+          ledState[tempY][tempX].previousLoc = DOWN;
+        }
+        tempY++;
       }
     }
-  else {
+
+    }
+  else if (state == apple) {
+    Serial.print("\nfwefegegvewgewbgewgew");
     userX++;
-    ledState[userY][userX - 1].state = true;
-    ledState[userY][userX].state = true;
+    ledState[userY][userX - 1].state = snake;
+    ledState[userY][userX].state = snake;
     ledState[userY][userX].previousLoc = LEFT;
 
-    ledState[userY][userX].previous = &ledState[userY][userX - 1];
-    ledState[userY][userX - 1].next = &ledState[userY][userX];
-
     length++;
   }
 
-  ledState[userY][userX].nextStep = RIGHT;
+  else {
+    return true;
+  }
+
   print_location(userY, userX);
   printLedStates();
+  return false;
 }
 
-void go_left() {
-  userDirection = get_user_direction();
-  if (userDirection == RIGHT) {
-    return;
-  }
-  if (!isPoint(userY, userX - 1)) {
-    if (userDirection == LEFT) {
-      for(int i=0; i < length; i++) {
-        switchPoints(userX + i, userY, userX + i - 1, userY);
-      }
-    }
-    
-  }
-  else {
-    length++;
-  }
-  userX--;
-  ledState[userY][userX].nextStep = LEFT;
-  // print_location(userY, userX);
-}
+bool go_left() {
 
-void go_up() {
-  userDirection = get_user_direction();
-  if (userDirection == DOWN) {
-    return;
+  if (userX == 0) {
+    return true;
   }
-  
-  if (!isPoint(userY - 1, userX)) {
-    if (userDirection == UP){
-      for(int i=0 ; i < length; i++) {
-        switchPoints(userX, userY + i, userX, userY + i - 1);
-      }
+
+  byte state = getState(userY, userX - 1);
+
+  if (!state) {
+    switchPoints(userX, userY, userX - 1, userY);
+    int tempY = userY, tempX = userX;
+    userX--;
+    if (ledState[userY][userX].previousLoc) {
+      ledState[userY][userX].previousLoc = RIGHT;
     }
 
+    for(int i= 1; i < length; i++) {
+
+      if (ledState[tempY][tempX].previousLoc == RIGHT) {
+        switchPoints(tempX + 1, tempY, tempX, tempY);
+        if (ledState[tempY][tempX].previousLoc) {
+          ledState[tempY][tempX].previousLoc = RIGHT;
+        }
+        tempX++;
+      }
+
+      else if (ledState[tempY][tempX].previousLoc == LEFT) {
+        switchPoints(tempX - 1, tempY, tempX, tempY);
+        if (ledState[tempY][tempX].previousLoc) {
+          ledState[tempY][tempX].previousLoc = LEFT;
+        }
+        tempX--;
+      }
+
+      else if (ledState[tempY][tempX].previousLoc == UP) {
+        switchPoints(tempX, tempY - 1, tempX, tempY);
+        if (ledState[tempY][tempX].previousLoc) {
+          ledState[tempY][tempX].previousLoc = UP;
+        }
+        tempY--;
+      }
+
+      else if(ledState[tempY][tempX].previousLoc == DOWN) {
+        switchPoints(tempX, tempY + 1, tempX, tempY);
+        if (ledState[tempY][tempX].previousLoc) {
+          ledState[tempY][tempX].previousLoc = DOWN;
+        }
+        tempY++;
+      }
+    }
+
+    }
+  else if (state == apple) {
+    Serial.print("\nfwefegegvewgewbgewgew");
+    userX--;
+    ledState[userY][userX + 1].state = snake;
+    ledState[userY][userX].state = snake;
+    ledState[userY][userX].previousLoc = RIGHT;
+
+    length++;
   }
 
   else {
-    length++;
+    return true;
   }
-  userY--;
-  ledState[userY][userX].nextStep = UP;
+
+  print_location(userY, userX);
+  printLedStates();
+  return false;
 }
 
-void go_down() {
-  byte userDirection = get_user_direction();
+bool go_up() {
 
-  if (userDirection == STABLE) {
-    userDirection = DOWN;
+  if (userY == 0) {
+    return true;
   }
 
-  if (userDirection == UP) {
-    return;
+  byte state = getState(userY - 1, userX);
+
+  if (!state) {
+    switchPoints(userX, userY, userX, userY - 1);
+    int tempY = userY, tempX = userX;
+    userY--;
+    if (ledState[userY][userX].previousLoc) {
+      ledState[userY][userX].previousLoc = DOWN;
+    }
+
+    for(int i= 1; i < length; i++) {
+      if (ledState[tempY][tempX].previousLoc == LEFT) {
+        switchPoints(tempX - 1, tempY, tempX, tempY);
+        if (ledState[tempY][tempX].previousLoc) {
+          ledState[tempY][tempX].previousLoc = LEFT;
+        }
+        tempX--;
+      }
+
+      else if (ledState[tempY][tempX].previousLoc == RIGHT) {
+        switchPoints(tempX + 1, tempY, tempX, tempY);
+        if (ledState[tempY][tempX].previousLoc) {
+          ledState[tempY][tempX].previousLoc = RIGHT;
+        }
+        tempX++;
+      }
+
+      else if (ledState[tempY][tempX].previousLoc == UP) {
+        switchPoints(tempX, tempY - 1, tempX, tempY);
+        if (ledState[tempY][tempX].previousLoc) {
+          ledState[tempY][tempX].previousLoc = UP;
+        }
+        tempY--;
+      }
+
+      else if(ledState[tempY][tempX].previousLoc == DOWN) {
+        switchPoints(tempX, tempY + 1, tempX, tempY);
+        if (ledState[tempY][tempX].previousLoc) {
+          ledState[tempY][tempX].previousLoc = DOWN;
+        }
+        tempY++;
+      }
+    }
+
+    }
+  else if (state == apple){
+    Serial.print("\nfwefegegvewgewbgewgew");
+    userY--;
+    ledState[userY + 1][userX].state = snake;
+    ledState[userY][userX].state = snake;
+    ledState[userY][userX].previousLoc = DOWN;
+
+    length++;
   }
-  
-  if (!isPoint(userY + 1, userX)) {
+
+  else {
+    return true;
+  }
+
+  print_location(userY, userX);
+  printLedStates();
+  return false;
+}
+
+bool go_down() {
+  if (userY == 7) {
+    return true;
+  }
+
+  byte state = getState(userY + 1, userX);
+
+  if (!state) {
     switchPoints(userX, userY, userX, userY + 1);
     int tempY = userY, tempX = userX;
     userY++;
@@ -335,6 +463,14 @@ void go_down() {
         tempX--;
       }
 
+      else if (ledState[tempY][tempX].previousLoc == RIGHT) {
+        switchPoints(tempX + 1, tempY, tempX, tempY);
+        if (ledState[tempY][tempX].previousLoc) {
+          ledState[tempY][tempX].previousLoc = RIGHT;
+        }
+        tempX++;
+      }
+
       else if (ledState[tempY][tempX].previousLoc == UP) {
         switchPoints(tempX, tempY - 1, tempX, tempY);
         if (ledState[tempY][tempX].previousLoc) {
@@ -342,35 +478,158 @@ void go_down() {
         }
         tempY--;
       }
-    }
 
-      if(ledState[userY][userX].previous) {
-        Serial.println(ledState[userY][userX].previous->next->state);
+      else if(ledState[tempY][tempX].previousLoc == DOWN) {
+        switchPoints(tempX, tempY + 1, tempX, tempY);
+        if (ledState[tempY][tempX].previousLoc) {
+          ledState[tempY][tempX].previousLoc = DOWN;
+        }
+        tempY++;
       }
     }
-  else {
-    userY++;
-    ledState[userY - 1][userX].state = true;
-    ledState[userY][userX].state = true;
-    ledState[userY][userX].previousLoc = UP;
 
-    ledState[userY][userX].previous = &ledState[userY - 1][userX];
-    ledState[userY - 1][userX].next = &ledState[userY][userX];
+  }
+
+  else if (state == apple){
+    Serial.print("\nfwefegegvewgewbgewgew");
+    userY++;
+    ledState[userY - 1][userX].state = snake;
+    ledState[userY][userX].state = snake;
+    ledState[userY][userX].previousLoc = UP;
 
     length++;
   }
-  ledState[userY][userX].nextStep = DOWN;
+
+  else {
+    return true;
+  }
+
   print_location(userY, userX);
   printLedStates();
+  return false;
 }
 
-bool isPoint(int userY, int userX) {
+byte getState(int userY, int userX) {
   return ledState[userY][userX].state;
 }
 
-byte get_user_direction() {
-  return userDirection = ledState[userY][userX].nextStep;
+void printGameOver() {
+  // Clear the LED matrix
+  clearMatrix();
+
+  // Define a simple "Game Over" pattern (each row is an 8-bit representation)
+  byte G[8] = {
+    B11111100,  // Row 1
+    B10000000,  // Row 2
+    B10000000,  // Row 3
+    B10011110,  // Row 4
+    B10000010,  // Row 5
+    B10000010,  // Row 6
+    B10000010,  // Row 7
+    B11111100   // Row 8
+  };
+
+  byte A[8] = {
+    B01111100,  // Row 1
+    B10000010,  // Row 2
+    B10000010,  // Row 3
+    B11111110,  // Row 4
+    B10000010,  // Row 5
+    B10000010,  // Row 6
+    B10000010,  // Row 7
+    B10000010   // Row 8
+  };
+
+  byte M[8] = {
+    B10000010,  // Row 1
+    B11000110,  // Row 2
+    B10101010,  // Row 3
+    B10010010,  // Row 4
+    B10000010,  // Row 5
+    B10000010,  // Row 6
+    B10000010,  // Row 7
+    B10000010   // Row 8
+  };
+
+  byte E[8] = {
+    B11111100,  // Row 1
+    B10000000,  // Row 2
+    B10000000,  // Row 3
+    B11111100,  // Row 4
+    B10000000,  // Row 5
+    B10000000,  // Row 6
+    B10000000,  // Row 7
+    B11111100   // Row 8
+  };
+
+  byte O[8] = {
+    B01111100,  // Row 1
+    B10000010,  // Row 2
+    B10000010,  // Row 3
+    B10000010,  // Row 4
+    B10000010,  // Row 5
+    B10000010,  // Row 6
+    B10000010,  // Row 7
+    B01111100   // Row 8
+  };
+
+  byte V[8] = {
+    B10000010,  // Row 1
+    B10000010,  // Row 2
+    B10000010,  // Row 3
+    B10000010,  // Row 4
+    B10000010,  // Row 5
+    B10000010,  // Row 6
+    B01000100,  // Row 7
+    B00101000   // Row 8
+  };
+
+  byte R[8] = {
+    B11111100,  // Row 1
+    B10000010,  // Row 2
+    B10000010,  // Row 3
+    B11111100,  // Row 4
+    B10010000,  // Row 5
+    B10001000,  // Row 6
+    B10000100,  // Row 7
+    B10000010   // Row 8
+  };
+
+  clearMatrix();
+
+  // Display "GAME OVER" letter by letter
+  displayLetter(G, 0);  // 'G'
+  delay(500);  // Wait for half a second
+  displayLetter(A, 1);  // 'A'
+  delay(500);
+  displayLetter(M, 2);  // 'M'
+  delay(500);
+  displayLetter(E, 3);  // 'E'
+  delay(500);
+  displayLetter(O, 4);  // 'O'
+  delay(500);
+  displayLetter(V, 5);  // 'V'
+  delay(500);
+  displayLetter(E, 6);  // 'E'
+  delay(500);
+  displayLetter(R, 7);  // 'R'
+  delay(2000);  // Keep the message for a while
 }
+
+void displayLetter(byte letter[8], int matrixColumn) {
+  for (int i = 0; i < 8; i++) {
+    lc.setRow(0, i, letter[i]);  // Display the letter
+  }
+}
+
+void clearMatrix() {
+    for (int row = 0; row < 8; row++) {
+        for (int col = 0; col < 8; col++) {
+            lc.setLed(0, row, col, false);
+        }
+    }
+}
+
 
 void print_location(int userY, int userX) {
   Serial.print("( ");
